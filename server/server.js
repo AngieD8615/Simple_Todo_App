740a66993abc3b61d50bd6ffe55f1c48d73b27b0
AngieD8615 <adavidson1@gmail.com>
2020-11-23 19:57:20 -0700

    spin up repo

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = 3010;
+
+
+app.use(express.static(path.join(__dirname, "/../client")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// app.get("/", (req, res) => {
+//   res.send("hello world from server")
+// })
+
+app.listen(PORT, () => {
+  console.log("serving on port: " + PORT);
+})
