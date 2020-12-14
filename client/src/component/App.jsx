f1308460dffeb8d3ec62199b3230f158ae472153
AngieD8615 <adavidson1@gmail.com>
2020-12-13 20:27:29 -0700

    ensuring all file are in git

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import AppBar from "./AppBar.jsx";
+import SideBar from "./SideBar.jsx";
+import ProjectFeed from "./ProjectsFeed.jsx";
+import "../style.css";
+import Grid from '@material-ui/core/Grid';
 
-export default function App() {
-  return (
-    <div>
-      hello world from App
-    </div>
-  )
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="nav">
+          <AppBar />
+        </div>
+        <Grid 
+          container 
+          direction="row" 
+          className="body"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <Grid item md={3} className="side-bar">
+            <SideBar />
+          </Grid>
+          <Grid item md={8} className="feed" justify="center">
+            <ProjectFeed />
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
